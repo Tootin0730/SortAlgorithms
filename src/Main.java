1e0a407598ec029fe2db52b0ef4e8843b98735f6
Tootin0730 <yisan1004@gmail.com>
2023-05-17 10:53:30 +0900

    Sort Algorithm by Selection Sort

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,16 @@ public class Main {
         int[] intArray = {20, 35, -15, 7, 55, 1, -22};
         for (int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex > 0;
              lastUnsortedIndex--){
-            for (int i = 0; i < lastUnsortedIndex; i++){
-                if (intArray[i] > intArray[i+1]){
-                    swap(intArray, i, i+1);
+            int largest = 0;
+
+            for (int i = 1; i <= lastUnsortedIndex; i++){
+                if (intArray[i] > intArray[largest]){
+                    largest = i;
                 }
             }
+            swap(intArray, largest, lastUnsortedIndex);
         }
-        for (int i = 0; i < intArray.length; i++){
+        for (int i = 0; i < intArray.length; i++) {
             System.out.println(intArray[i]);
         }
     }
