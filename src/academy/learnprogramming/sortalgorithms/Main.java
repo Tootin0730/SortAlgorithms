d5a79679cabfa35a36b96278b0807c9280210c36
Tootin0730 <yisan1004@gmail.com>
2023-05-20 16:54:17 +0900

    Recursion

--- src/academy/learnprogramming/sortalgorithms/Main.java
+++ src/academy/learnprogramming/sortalgorithms/Main.java
@@ -1,31 +1,33 @@
 package academy.learnprogramming.sortalgorithms;
 
 public class Main {
-
     public static void main(String[] args) {
-
-        int[] intArray = {20, 35, -15, 7, 55, 1, -22};
-
-        for (int gap = intArray.length / 2; gap > 0; gap /= 2) {
-
-            for (int i = gap; i < intArray.length; i++) {
-                int newElement = intArray[i];
-
-                int j = i;
-
-                while (j >= gap && intArray[j - gap] > newElement) {
-                    intArray[j] = intArray[j - gap];
-                    j -= gap;
-                }
-
-                intArray[j] = newElement;
-            }
-        }
-
-        for (int i = 0; i < intArray.length; i++) {
-            System.out.println(intArray[i]);
-        }
-
+        System.out.println(iterativeFactorial(3));
+        System.out.println(recursiveFactorial(3));
     }
 
+    // 1! = 1 * 0! = 1
+    // 2! = 2 * 1 = 2 * 1!
+    // 3! = 3 * 2 * 1 = 3 * 2!
+    // 4! = 4 * 3 * 2 * 1 = 4 * 3!
+
+    // n! = n *(n - 1)!
+
+    public static int recursiveFactorial(int num) {
+        if (num == 0) {
+            return 1;
+        }
+        return num * recursiveFactorial(num - 1);
+    }
+
+    public static int iterativeFactorial(int num) {
+        if (num == 0) {
+            return 1;
+        }
+        int factorial = 1;
+        for (int i = 1; i <= num; i++) {
+            factorial *= i;
+        }
+        return factorial;
+    }
 }
