7cba7ea0a7db3e9a2fa823de91d89a744ec359c9
Tootin0730 <yisan1004@gmail.com>
2023-05-26 11:42:13 +0900

    Sorting Arrays using the JDK

--- src/academy/learnprogramming/sortalgorithms/Main.java
+++ src/academy/learnprogramming/sortalgorithms/Main.java
@@ -1,41 +1,16 @@
 package academy.learnprogramming.sortalgorithms;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-        int[] radixArray = { 4725, 4586, 1330, 8792, 1594, 5729 };
-        radixSort(radixArray, 10, 4);
-        for (int i = 0; i < radixArray.length; i++) {
-            System.out.println(radixArray[i]);
-        }
-    }
+        int[] intArray = {20, 32, -15, 7, 55, 1, -22};
 
-    public static void radixSort(int[] input, int radix, int width) {
-        for (int i = 0; i < width; i++) {
-            radixSingleSort(input, i, radix);
-        }
-    }
+        Arrays.parallelSort(intArray);
 
-    public static void radixSingleSort(int[] input, int position, int radix) {
-        int numItems = input.length;
-        int[] countArray = new int[radix];
-        for (int value: input) {
-            countArray[getDigit(position, value, radix)]++;
+        for (int i = 0; i < intArray.length; i++){
+            System.out.println(intArray[i]);
         }
-        for (int j = 1; j < radix; j++) {
-            countArray[j] += countArray[j - 1];
-        }
-        int[] temp = new int[numItems];
-        for (int tempIndex = numItems - 1; tempIndex >= 0; tempIndex--) {
-            temp[--countArray[getDigit(position, input[tempIndex], radix)]] =
-                    input[tempIndex];
-        }
-        for (int tempIndex = 0; tempIndex < numItems; tempIndex++) {
-            input[tempIndex] = temp[tempIndex];
-        }
-    }
-
-    public static int getDigit(int position, int value, int radix) {
-        return value / (int) Math.pow(radix, position) % radix;
     }
 }
