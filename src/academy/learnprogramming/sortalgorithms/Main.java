a794e7c4cc5c35297fae004a36a634f9ccb47ce8
Tootin0730 <yisan1004@gmail.com>
2023-05-27 09:10:01 +0900

    Sorting Algorithm challenge #2 (recursion)

--- src/academy/learnprogramming/sortalgorithms/Main.java
+++ src/academy/learnprogramming/sortalgorithms/Main.java
@@ -4,39 +4,42 @@ public class Main {
 
     public static void main(String[] args) {
         int[] intArray = { 20, 35, -15, 7, 55, 1, -22 };
-
-        mergeSort(intArray, 0, intArray.length);
-
-        for (int i = 0; i < intArray.length; i++){
+        insertionSort(intArray, intArray.length);
+//        for (int firstUnsortedIndex = 1; firstUnsortedIndex < intArray.length;
+//             firstUnsortedIndex++) {
+//            int newElement = intArray[firstUnsortedIndex];
+//
+//            int i;
+//
+//            for (i = firstUnsortedIndex; i > 0 && intArray[i - 1] > newElement; i--) {
+//                intArray[i] = intArray[i - 1];
+//            }
+//
+//            intArray[i] = newElement;
+//        }
+        for (int i = 0; i < intArray.length; i++) {
             System.out.println(intArray[i]);
         }
     }
 
-    public static void mergeSort(int[] input, int start, int end){
-        if (end - start < 2){
+    public static void insertionSort(int[] input, int numItems) {
+
+        if (numItems < 2) {
             return;
         }
-        int mid = (start + end) / 2;
-        mergeSort(input, start, mid);
-        mergeSort(input, mid, end);
-        merge(input, start, mid, end);
-    }
-
-    public static void merge(int[] input, int start, int mid, int end){
-        if (input[mid - 1] >= input[mid]){
-            return;
+        insertionSort(input, numItems - 1);
+        int newElement = input[numItems - 1];
+        int i;
+        for (i = numItems - 1; i > 0 && input[i - 1] > newElement; i--) {
+            input[i] = input[i - 1];
         }
-
-        int i = start;
-        int j = mid;
-        int tempIndex = 0;
-
-        int[] temp = new int[end - start];
-        while (i < mid && j < end){
-            temp[tempIndex++] = input[i] >= input[j] ? input[i++] : input[j++];
+        input[i] = newElement;
+        System.out.println("Result of call when numItems = " + numItems);
+        for (i = 0; i < input.length; i++) {
+            System.out.print(input[i]);
+            System.out.print(", ");
         }
-
-        System.arraycopy(input, i, input, start + tempIndex, mid - i);
-        System.arraycopy(temp, 0, input, start, tempIndex);
+        System.out.println("");
+        System.out.println("---------------------");
     }
 }
